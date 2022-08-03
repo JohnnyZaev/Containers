bb10bfed6b42d940141e118bf253c4370ed5b602
gvarys <johnnyzaev@gmail.com>
2022-08-03 11:03:39 +0300

    vector includes added

--- vector.hpp
+++ vector.hpp
@@ -5,6 +5,9 @@
 #ifndef CONTAINERS_VECTOR_HPP
 #define CONTAINERS_VECTOR_HPP
 
-#include <iostream>
+# include <memory>
+# include <cstddef>
+# include <iostream>
+# include <stdexcept>
 
 #endif //CONTAINERS_VECTOR_HPP
